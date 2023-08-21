544e60e0676b6622fe3a6b27615d036c8641e6a3
ChiYMY <1449271571@qq.com>
2023-08-21 13:17:21 +0800

    设置图片预加载

--- about/js/index.js
+++ about/js/index.js
@@ -7,48 +7,157 @@ items.forEach((item) => {
 // 移除所有active，为当前元素添加active类
 function setActive() {
     items.forEach((item) => {
-        item.classList.remove("active")
+        item.classList.remove("active");
     })
-    this.classList.add("active")
+    this.classList.add("active");
 }
 
 // 数组储存css的url的位置
-var bgs = new Array('url("about/images/bgs/bg01.jpg")', 'url("about/images/bgs/bg02.jpg")', 'url("about/images/bgs/bg04.jpg")', 'url("about/images/bgs/bg05.jpg")', 'url("about/images/bgs/bg08.jpg")', 'url("about/images/bgs/bg25.jpg")', 'url("about/images/bgs/bg09.jpg")', 'url("about/images/bgs/bg10.jpg")', 'url("about/images/bgs/bg12.jpg")', 'url("about/images/bgs/bg13.jpg")', 'url("about/images/bgs/bg25.jpg")', 'url("about/images/bgs/bg15.jpg")', 'url("about/images/bgs/bg17.jpg")', 'url("about/images/bgs/bg19.jpg")', 'url("about/images/bgs/bg20.jpg")', 'url("about/images/bgs/bg21.jpg")', 'url("about/images/bgs/bg22.jpg")', 'url("about/images/bgs/bg23.jpg")', 'url("about/images/bgs/bg25.jpg")', 'url("about/images/bgs/bg26.jpg")', 'url("about/images/bgs/bg27.jpg")', 'url("about/images/bgs/bg28.jpg")', 'url("about/images/bgs/bg29.jpg")', 'url("about/images/bgs/bg30.jpg")', 'url("about/images/bgs/bg31.jpg")', 'url("about/images/bgs/bg32.jpg")', 'url("about/images/bgs/bg33.jpg")', 'url("about/images/bgs/bg34.jpg")', 'url("about/images/bgs/bg35.jpg")', 'url("about/images/bgs/bg36.jpg")', 'url("about/images/bgs/bg37.jpg")', 'url("about/images/bgs/bg38.jpg")', 'url("about/images/bgs/bg39.jpg")', 'url("about/images/bgs/bg40.jpg")', 'url("about/images/bgs/bg41.jpg")', 'url("about/images/bgs/bg42.jpg")', 'url("about/images/bgs/bg43.jpg")', 'url("about/images/bgs/bg44.jpg")', 'url("about/images/bgs/bg45.jpg")', 'url("about/images/bgs/bg46.jpg")', 'url("about/images/bgs/bg47.jpg")', 'url("about/images/bgs/bg48.jpg")', 'url("about/images/bgs/bg49.jpg")', 'url("about/images/bgs/bg50.jpg")', 'url("about/images/bgs/bg51.jpg")', 'url("about/images/bgs/bg52.jpg")', 'url("about/images/bgs/bg53.jpg")', 'url("about/images/bgs/bg54.jpg")', 'url("about/images/bgs/bg55.jpg")', 'url("about/images/bgs/bg56.jpg")', 'url("about/images/bgs/bg57.jpg")', 'url("about/images/bgs/bg58.jpg")', 'url("about/images/bgs/bg59.jpg")', 'url("about/images/bgs/bg60.jpg")', 'url("about/images/bgs/bg61.jpg")');
+var bgs = new Array(
+    'url("about/images/bgs/bg01.jpg")',
+    'url("about/images/bgs/bg02.jpg")',
+    'url("about/images/bgs/bg03.jpg")',
+    'url("about/images/bgs/bg04.jpg")',
+    'url("about/images/bgs/bg05.jpg")',
+    'url("about/images/bgs/bg06.jpg")',
+    'url("about/images/bgs/bg07.jpg")',
+    'url("about/images/bgs/bg08.jpg")',
+    'url("about/images/bgs/bg09.jpg")',
+    'url("about/images/bgs/bg10.jpg")',
+    'url("about/images/bgs/bg11.jpg")',
+    'url("about/images/bgs/bg12.jpg")',
+    'url("about/images/bgs/bg13.jpg")',
+    'url("about/images/bgs/bg14.jpg")',
+    'url("about/images/bgs/bg15.jpg")',
+    'url("about/images/bgs/bg16.jpg")',
+    'url("about/images/bgs/bg17.jpg")',
+    'url("about/images/bgs/bg18.jpg")',
+    'url("about/images/bgs/bg19.jpg")',
+    'url("about/images/bgs/bg20.jpg")',
+    'url("about/images/bgs/bg21.jpg")',
+    'url("about/images/bgs/bg22.jpg")',
+    'url("about/images/bgs/bg23.jpg")',
+    'url("about/images/bgs/bg24.jpg")',
+    'url("about/images/bgs/bg25.jpg")',
+    'url("about/images/bgs/bg26.jpg")',
+    'url("about/images/bgs/bg27.jpg")',
+    'url("about/images/bgs/bg28.jpg")',
+    'url("about/images/bgs/bg29.jpg")',
+    'url("about/images/bgs/bg30.jpg")',
+    'url("about/images/bgs/bg31.jpg")',
+    'url("about/images/bgs/bg32.jpg")',
+    'url("about/images/bgs/bg33.jpg")',
+    'url("about/images/bgs/bg34.jpg")',
+    'url("about/images/bgs/bg35.jpg")',
+    'url("about/images/bgs/bg36.jpg")',
+    'url("about/images/bgs/bg37.jpg")',
+    'url("about/images/bgs/bg38.jpg")',
+    'url("about/images/bgs/bg39.jpg")',
+    'url("about/images/bgs/bg40.jpg")',
+    'url("about/images/bgs/bg41.jpg")',
+    'url("about/images/bgs/bg42.jpg")',
+    'url("about/images/bgs/bg43.jpg")',
+    'url("about/images/bgs/bg44.jpg")',
+    'url("about/images/bgs/bg45.jpg")',
+    'url("about/images/bgs/bg46.jpg")',
+    'url("about/images/bgs/bg47.jpg")',
+    'url("about/images/bgs/bg48.jpg")',
+    'url("about/images/bgs/bg49.jpg")',
+    'url("about/images/bgs/bg50.jpg")',
+    'url("about/images/bgs/bg51.jpg")',
+    'url("about/images/bgs/bg52.jpg")',
+    'url("about/images/bgs/bg53.jpg")',
+    'url("about/images/bgs/bg54.jpg")',
+    'url("about/images/bgs/bg55.jpg")',
+    'url("about/images/bgs/bg56.jpg")',
+    'url("about/images/bgs/bg57.jpg")',
+    'url("about/images/bgs/bg58.jpg")',
+    'url("about/images/bgs/bg59.jpg")',
+    'url("about/images/bgs/bg60.jpg")',
+    'url("about/images/bgs/bg61.jpg")',
+    'url("about/images/bgs/bg62.jpg")',
+    'url("about/images/bgs/bg63.jpg")',
+    'url("about/images/bgs/bg64.jpg")',
+    'url("about/images/bgs/bg65.jpg")',
+    'url("about/images/bgs/bg66.jpg")',
+    'url("about/images/bgs/bg67.jpg")'
+);
+// 设置预加载组
+var bgs_ready = [];
+var bgs_src=[];
+for (let i = 0, j = 0; i < 5; i++) {
+    let str =bgs[Math.floor(Math.random() * (bgs.length - 1 - 0 + 1)) + 0];
+    let newstr = str.replace(/url\(\"\s*([^)]*)\s*\"\)/, "$1");
+    bgs_ready[i] = bgs[Math.floor(Math.random() * (bgs.length - 1 - 0 + 1)) + 0];
+    bgs_src.push(newstr)
+}
+//开始预加载
+loadStart();
+// promise实现异步加载，异步调用
+function loadImg(src) {
+    console.log('src :>> ', src);
+    let p = new Promise(function (resolve, reject) {
+        let img = new Image();
+        img.onload=()=>{
+            console.log('src加载为 :>> ', src);
+            resolve(img);
+        }
+        img.onerror=()=>{
+            reject(src);
+        }
+        img.src=src;
+    });
+    console.log('p :>> ', p);
+    return p;
+}
+// 设置async异步函数
+async function loadStart(){
+    for (let i = 0; i < bgs_src.length; i++) {
+        await loadImg(bgs_src[i]).then(() => {
+            console.log('bgs_ready :>> ', bgs_src[i]);
+        }).catch((err) => {
+            console.log('图片加载出错 :>> ');
+        });
+    }
+}
+
+
+
 // 随机更换
 function changeBg() {
-    document.getElementById('bgid').style.background = bgs[Math.floor(Math.random() * (bgs.length - 1 - 0 + 1)) + 0] + "no-repeat center fixed";
+    document.getElementById('bgid').style.background = bgs_ready[Math.floor(Math.random() * (bgs_ready.length - 1 - 0 + 1)) + 0] + "no-repeat center fixed";
     document.getElementById('bgid').style.backgroundSize = "cover";
 }
 
-var lttime = document.getElementById("lttime")
-var clock_p = document.getElementById("clock_p")
+var lttime = document.getElementById("lttime");
+var clock_p = document.getElementById("clock_p");
 
 function timeSetRight() {
-    var time = new Date()
-    year = time.getFullYear()
-    month = time.getMonth()
-    day = time.getDate()
-    hour = time.getHours()
-    minutes = time.getMinutes()
-    s = time.getSeconds()
-    week = ["日", "月", "火", "水", "木", "金", "土"]
-    document.getElementById("rtbox").innerHTML = year + '-' + month + '-' + day + '  ' + week[time.getDay()]
+    var time = new Date();
+    year = time.getFullYear();
+    month = time.getMonth();
+    day = time.getDate();
+    hour = time.getHours();
+    minutes = time.getMinutes();
+    s = time.getSeconds();
+    week = ["日", "月", "火", "水", "木", "金", "土"];
+    document.getElementById("rtbox").innerHTML = year + '-' + month + '-' + day + '  ' + week[time.getDay()];
     if (hour < 10) {
-        hour = '0' + hour
+        hour = '0' + hour;
     }
     if (minutes < 10) {
-        minutes = '0' + minutes
+        minutes = '0' + minutes;
     }
     if (s < 10) {
-        s = '0' + s
+        s = '0' + s;
     }
     if (hour < 12) {
-        ampm = 'AM'
+        ampm = 'AM';
     } else {
-        ampm = 'PM'
+        ampm = 'PM';
     }
-    lttime.innerHTML = ampm + '  ' + hour + ':' + minutes + ':' + s
-    clock_p.innerHTML = hour + ':' + minutes + ':' + s
+    lttime.innerHTML = ampm + '  ' + hour + ':' + minutes + ':' + s;
+    clock_p.innerHTML = hour + ':' + minutes + ':' + s;
 }
 // 定时刷新时间，更换壁纸
 setInterval(timeSetRight, 1000);
@@ -60,40 +169,40 @@ function getFocus(foid) {
     let tul = document.getElementById("footul");
     let tt = document.getElementById("topbtn");
     let cc = document.querySelector(".change");
+    function Focusadd() {
+        nn.classList.add("stopfoot");
+        tul.classList.add("stopfootul");
+        tt.classList.add("stopfoot");
+        cc.classList.add("topbtnspin");
+    }
+    function Focusremove() {
+        nn.classList.remove("stopfoot");
+        tul.classList.remove("stopfootul");
+        tt.classList.remove("stopfoot");
+        cc.classList.remove("topbtnspin");
+    }
     if (getFocus_flag == 'noed') {
-        if (foid=='p1') {
-            nn.classList.add("stopfoot");
-            tul.classList.add("stopfootul");
-            tt.classList.add("stopfoot");
-            cc.classList.add("topbtnspin");
-            getFocus_flag = getFocus_flag+foid;
+        if (foid == 'p1') {
+            Focusadd();
+            getFocus_flag = getFocus_flag + foid;
         }
-        if (foid=='p2') {
-            nn.classList.add("stopfoot");
-            tul.classList.add("stopfootul");
-            tt.classList.add("stopfoot");
-            cc.classList.add("topbtnspin");
-            tt.style.cursor='url("about/images/cur/no.cur"),auto';
-            getFocus_flag = getFocus_flag+foid;
+        if (foid == 'p2') {
+            Focusadd();
+            tt.style.cursor = 'url("about/images/cur/no.cur"),auto';
+            getFocus_flag = getFocus_flag + foid;
         }
     }
-    else{
-        if (getFocus_flag=='noed'+'p1'&&foid=='p1'&&this.menu_flag!=false) {
-            nn.classList.remove("stopfoot");
-            tul.classList.remove("stopfootul");
-            tt.classList.remove("stopfoot");
-            cc.classList.remove("topbtnspin");
+    else {
+        if (getFocus_flag == 'noed' + 'p1' && foid == 'p1' && menu_flag == true) {
+            Focusremove();
             getFocus_flag = 'noed';
         }
-        if (getFocus_flag == 'noed'+'p2'&&foid=='p2') {
-            nn.classList.remove("stopfoot");
-            tul.classList.remove("stopfootul");
-            tt.classList.remove("stopfoot");
-            cc.classList.remove("topbtnspin");
-            tt.style.cursor='url("about/images/cur/hand.cur"),auto';
+        if (getFocus_flag == 'noed' + 'p2' && foid == 'p2') {
+            Focusremove();
+            tt.style.cursor = 'url("about/images/cur/hand.cur"),auto';
             getFocus_flag = 'noed';
         }
-    } 
+    }
 }
 
 //  显示弹出框
@@ -109,9 +218,15 @@ function menu_show() {
         menu_flag = true;
         this.getFocus('p2');
     }
+    if (menu_flag == false) {
+        document.getElementById("topbtn").style.cursor = 'url("about/images/cur/no.cur"),auto';
+    }
+    else {
+        document.getElementById("topbtn").style.cursor = 'url("about/images/cur/hand.cur"),auto';
+    }
 }
 
-function select_bgs() {  
-    
+function select_bgs() {
+
 }
 
